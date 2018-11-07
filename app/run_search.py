b66d7b07cbff885beee1516a923b99738c100a8c
spawnmarvel <espenkleivane@gmail.com>
2018-11-07 18:03:29 +0100

    More on search

--- app/run_search.py
+++ app/run_search.py
@@ -2,12 +2,14 @@ import requests
 # http://lucene.apache.org/solr/guide/7_5/the-standard-query-parser.html
 
 def get_id():
+    """Get id"""
     r = requests.get("http://localhost:8983/solr/newcore/select?q=id:1")
     print("Solr status " + str(r.status_code))
     js = r.json()
     print(js)
 
 def get_id_range():
+    """Get range"""
     r = requests.get("http://localhost:8983/solr/newcore/select?q=id:[9980 TO 9982]")
     print("Solr status " + str(r.status_code))
     js = r.json()
@@ -15,16 +17,45 @@ def get_id_range():
         print(l)
 
 def get_string():
-    r = requests.get("http://localhost:8983/solr/newcore/select?q=desc:*Gas*&rows=2000")
+    """Get 7 rows where desc has Gas"""
+    r = requests.get("http://localhost:8983/solr/newcore/select?q=desc:*Gas*&rows=7")
+    print("Solr status " + str(r.status_code))
+    js = r.json()
+    for l in js["response"]["docs"]:
+        print(l)
+
+def get_string_sort():
+    """Get default rows, sort by id desc """
+    r = requests.get("http://localhost:8983/solr/newcore/select?q=*:*&sort=id desc")
+    print("Solr status " + str(r.status_code))
+    js = r.json()
+    for l in js["response"]["docs"]:
+        print(l)
+
+def get_string_sort_desc():
+    """Get 3 rows where desc start with Oil, sort by id asc"""
+    r = requests.get("http://localhost:8983/solr/newcore/select?q=desc:Oil*&sort=id asc&rows=3")
+    print("Solr status " + str(r.status_code))
+    js = r.json()
+    for l in js["response"]["docs"]:
+        print(l)
+
+def get_name_sort_desc_():
+    """Get 3 rows, just name and id, sort by id asc"""
+    r = requests.get("http://localhost:8983/solr/newcore/select?fl=tag id&q=*:*&rows=3&sort=id desc")
     print("Solr status " + str(r.status_code))
     js = r.json()
     for l in js["response"]["docs"]:
         print(l)
 
 
+
 def main():
     # get_id()
     # get_id_range()
-    get_string()
+    # get_string_sort()
+    # get_string_sort_desc()
+    get_name_sort_desc_()
+   
 if __name__ == "__main__":
     main()
