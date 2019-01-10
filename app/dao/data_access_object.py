22d2d0e355446ec4baffe69bf5fb9ea9027e86d9
spawnmarvel <espenkleivane@gmail.com>
2019-01-10 23:39:04 +0100

    Added DataAccessObject for interface 2

--- app/dao/data_access_object.py
+++ app/dao/data_access_object.py
@@ -5,17 +5,12 @@ class DataAccess:
     def __init__(self):
         pass
 
-    def search_string(self, user_input):
-        """Get 10000 rows where desc has Gas"""
-        url = "http://localhost:8983/solr/newcore/select?q=desc:*"+ str(user_input) + "*&rows=10000"
-        print(url)
-        # r = requests.get("http://localhost:8983/solr/newcore/select?q=desc:*Gas*&rows=7")
+    def search_string(self, user_input, atr_input):
+        """Get 10000 rows where atr like input"""
+        url = "http://localhost:8983/solr/newcore/select?q=" + atr_input + ":*"+ str(user_input) + "*&rows=10000"
         r = requests.get(url)
-        print("Solr status " + str(r.status_code))
+        # print("Solr status " + str(r.status_code))
         js = r.json()
-        for l in js["response"]["docs"]:
-            # print(l)
-            pass
         li = js["response"]["docs"]
         return li
 
--- app/search/views.py
+++ app/search/views.py
@@ -8,21 +8,24 @@ from app.dao import data_access_object
 @search.route("/search_str", methods=["GET", "POST"])
 def search_str():
     rv = ""
+    param = ""
     data = ""
     req = "GET"
     local_url_selected = views.get_url_select()
     if request.method == "POST":
         print("post")
         req = "POST"
-        if request.form["action"] == "search_input":
+        if request.form["action"] == "Search":
             user_input = request.form["user_input"]
             rv = user_input
+            param = request.form["options"]
+
             if len(rv) < 2:
                 data = ["Msg", "Blank input"]
             else:
                 try:
-                    data = data_access_object.DataAccess().search_string(rv)
+                    data = data_access_object.DataAccess().search_string(rv, param)
                 except Exception as e:
                     data = ["error", str(e)]
     #GET
-    return render_template("search.html", rv=rv,req=req, local_url_selected=local_url_selected, data=data)
+    return render_template("search.html", rv=rv,req=req,param=param, local_url_selected=local_url_selected, data=data)
